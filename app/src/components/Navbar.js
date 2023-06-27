87d60e5695f358c053ff0b43d54295ce00d2bd0b
Kartik <abheytyagi010@gmail.com>
2023-06-27 22:39:57 +0530

    Navbar

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from 'react-router-dom';
+import Logo from '../images/logo.png';
 
 const Navbar = () => {
   return (
     <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <NavLink className="navbar-brand" to="#">Navbar</NavLink>
+  <img src={Logo} alt='Logo' width={100} height={100}/>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
